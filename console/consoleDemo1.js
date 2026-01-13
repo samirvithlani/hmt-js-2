766b4cbef7f3e6d3326d0d233fdc5720341f362e
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-13 11:27:53 +0530

    console

---
+++ console/consoleDemo1.js
@@ -0,0 +1,18 @@
+//log()
+console.log("javascript loaded...")
+//warn
+console.warn("warning...")
+//error
+console.error("error....")
+
+//var let const..
+
+var x =100
+console.log("value of x = ",x)
+var y = 200
+console.log("value of y = ",y)
+
+var z = x+y
+console.log("value of z = ",z)
+
+
