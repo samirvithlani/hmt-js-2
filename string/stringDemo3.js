374125c64494a0b021ec2fcab21ab154b8c35021
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-29 12:32:11 +0530

    code

--- string/stringDemo3.js
+++ string/stringDemo3.js
@@ -18,7 +18,25 @@ console.log(String.fromCharCode(x))
 
 //a -97 -->A 97-32 -65
 
-var user = "amit"
+var user = "amIt"
 var upperUser = ""
 
 //AMIT
+
+for(let i=0;i<user.length;i++){
+
+    
+    var ch = user.charCodeAt(i) //97 //108 
+    if(ch>=97 && ch<=121){
+        ch = ch-32 //65
+    }
+    //console.log(ch)
+    //console.log(String.fromCharCode(ch))
+    upperUser = upperUser + String.fromCharCode(ch) //65
+
+}
+console.log(upperUser)
+
+
+
+
